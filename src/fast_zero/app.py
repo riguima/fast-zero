b97cdcc9f17605f893a7d50ccf3a3eb3e3ef634f
riguima <richard.alexsander.guima@gmail.com>
2025-03-06 09:01:35 -0300

    Aula 02

--- src/fast_zero/app.py
+++ src/fast_zero/app.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/', response_model=Message)
 def read_root():
-    return {"message": "Hello World"}
+    return {'message': 'Hello World'}
+
+
+@app.get('/html', response_class=HTMLResponse)
+def read_root_html():
+    return '<h1>Hello World</h1>'
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,10 @@ def test_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Hello World'}
+
+
+def test_root_html_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)
+    response = client.get('/html')
+    assert response.status_code == HTTPStatus.OK
+    assert response.text == '<h1>Hello World</h1>'
